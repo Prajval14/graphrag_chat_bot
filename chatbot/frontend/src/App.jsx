e9c67050c2e76acf4428503383c3a145e40a18b7
Prajval Patel <prajval1420@gmail.com>
2025-05-19 15:27:04 -0400

    removed unnecessary code

--- chatbot/frontend/src/App.jsx
+++ chatbot/frontend/src/App.jsx
@@ -1,55 +1,15 @@
-import React, { useState } from "react";
 import Chatbot from "./components/Chatbot";
-import GettingStartedForm from "./components/GettingStartedForm";
-import CredentialsForm from "./components/CredentialsForm";
-import "./index.css";
 
 function App() {
-  const [step, setStep] = useState(1); // 1 = Getting Started, 2 = Credentials
-  const [formData, setFormData] = useState({
-    url: "",
-    name: "",
-    iconUrl: null,
-    useGraphRag: false,
-    color: "#7c3aed",
-  });
-  const [creds, setCreds] = useState({
-    openai: "",
-    azure: "",
-    neo4j: "",
-  });
-
-  // Navigation between forms
-  const goToCredentials = () => setStep(2);
-  const goBack = () => setStep(1);
-
   return (
     <div className="main-layout">
-      <div className="left-column">
-        {step === 1 ? (
-          <GettingStartedForm
-            data={formData}
-            setData={setFormData}
-            onNext={goToCredentials}
-          />
-        ) : (
-          <CredentialsForm
-            data={creds}
-            setData={setCreds}
-            onBack={goBack}
-            onSubmit={() => console.log({ ...formData, ...creds })}
-          />
-        )}
-      </div>
-      <div className="right-column">
-        <h1>Live Preview:</h1>
         <Chatbot
-          botName={formData.name || "Chatbot"}
-          botIcon={formData.iconUrl}
-          botColor={formData.color}
+          botName={"Chatbot"}
+          botIcon={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5G_lXK5cROdWBjbkK6h3rghuHW1pcfFwDfg&s"}
+          botColor={"green"}
         />
-      </div>
     </div>
   );
 }
+
 export default App;
